ae4a43b797b29089fb5363cfb116d1493177f61f
mcronERNI <mhike.cron@betterask.erni>
2025-01-24 17:32:12 +0800

    initial TodoAPI

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public long ItemId { get; set; }
+        public string? Name { get; set; }
+        public string? Secret { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class TodoItemDTO
+    {
+        [Key]
+        public long ItemId { get; set; }
+        public string? Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
